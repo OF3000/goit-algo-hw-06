8b0c00eae4628e46a8d11d12991743b98b6c2cb4
OF3000 <ofpost@i.ua>
2024-05-19 18:49:58 +0300

    init

---
+++ 1.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+G.add_nodes_from(["Kyiv","Zhitomyr","Kmelnitskyi","Rivne","Ternopil","Lviv"])
+
+G.add_edges_from([("Kyiv","Zhitomyr"),("Zhitomyr","Kmelnitskyi"),("Zhitomyr","Rivne"),("Rivne","Ternopil"),("Rivne","Lviv"),
+                  ("Kmelnitskyi","Ternopil"),("Ternopil","Lviv")])
+
+nx.draw(G, with_labels=True)
+plt.show()
+
+print(f"Number of nodes: {G.number_of_nodes()}")
+print(f"Number of edges: {G.number_of_edges()}")
+
